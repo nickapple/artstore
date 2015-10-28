c7cdccda5d756854c5d687b992a146eb7f666d11
sdlong <sdlong.jeng@gmail.com>
2015-10-28 17:59:59 +0800

    修改 heroku domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'limitless-plains-4640.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'artstore-sample.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
